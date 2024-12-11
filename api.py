1efe04fc9fc940dbc1e7e99de9f07cee46ec6c7d
Israel Julian <cidseulmail.com>
2024-12-10 22:25:50 -0600

    Versión final de la práctica REST

--- api.py
+++ api.py
@@ -31,6 +31,11 @@ def alumnos(sesion:Session=Depends(generador_sesion)):
 def alumnos_por_id(id:int,sesion:Session=Depends(generador_sesion)):
     print("Api consultando alumnos por Id")
     return repo.alumnos_por_id(sesion, id)
+#para borrar alumnos por id
+@app.delete("/alumnos/{id}")
+def borrar_alumno(id:int, sesion:Session=Depends(generador_sesion)):
+    repo.borra_alumno_por_id(sesion,id)
+    return{"usuario borrado","ok"}
 
 #se hizo para consultar todas las fotos
 @app.get("/fotos")
@@ -44,10 +49,22 @@ def fotos_por_id(id:int,sesion:Session=Depends(generador_sesion)):
     return repo.fotos_por_id(sesion, id)
 
 #se hizo para consultar fotos por id_al
-@app.get("/fotos/alumnos/{id}")
+@app.get("/alumnos/{id}/fotos")
 def fotos_por_id_alumnos(id:int,sesion:Session=Depends(generador_sesion)):
-    print("Api consultando fotos por  Id_alumnos")
+    print("Api consultando fotos por  Id_alumnos",id)
     return repo.fotos_por_id_alumno(sesion, id)
+
+#para borrar fotos por id
+@app.delete("/fotos/{id}")
+def borrar_foto(id:int, sesion:Session=Depends(generador_sesion)):
+    repo.borra_foto_por_id(sesion,id)
+    return{"foto borrada","ok"}
+
+#para borrar fotos por id alumno
+@app.delete("/alumnos/{id}/fotos")
+def borrar_fotos_id_alumnos(id:int, sesion:Session=Depends(generador_sesion)):
+    repo.borrar_fotos_por_id_alumno(sesion,id)
+    return{"foto borrada","ok"}
 
 
 
@@ -64,7 +81,18 @@ def calificaciones_por_id(id:int,sesion:Session=Depends(generador_sesion)):
     return repo.calificaciones_por_id(sesion, id)
 
 #se hizo para consultar calificaciones por id_al
-@app.get("/calificaciones/alumnos/{id}")
+@app.get("/alumnos/{id}/calificaciones")
 def calificaciones_por_id_alumno(id:int,sesion:Session=Depends(generador_sesion)):
-    print("Api consultando calificaciones por  Id_alumnos")
+    print("Api consultando calificaciones por  Id_alumnos",id)
     return repo.calificaciones_por_id_alumno(sesion, id)
+
+#para borrar calificaciones por id
+@app.delete("/calificaciones/{id}")
+def borrar_calificacion(id:int, sesion:Session=Depends(generador_sesion)):
+    repo.borra_calificaciones_por_id(sesion,id)
+    return{"calificacion borrada","ok"}
+#para borrar calificaciones por id alumno
+@app.delete("/alumnos/{id}/calificaciones")
+def borrar_calificaciones_id_alumnos(id:int, sesion:Session=Depends(generador_sesion)):
+    repo.borrar_calificaciones_por_id_alumno(sesion,id)
+    return{"calificación borrada","ok"}
--- orm/repo.py
+++ orm/repo.py
@@ -12,6 +12,28 @@ def alumnos_por_id(sesion:Session,id_al:int):
     print("select * from app.alumnos where id = ", id_al)
     return sesion.query(modelos.Alumnos).filter(modelos.Alumnos.id==id_al).first()
 
+#Delete '/alumnos/{id}'
+# delete from app.alumno where id=id_al
+def borra_alumno_por_id(sesion:Session, id_alumno:int):
+    print("delete from app.alumno where id=", id_alumno)
+    #1.-borrar fotos del alumno
+    borrar_fotos_por_id_alumno(sesion,id_alumno)
+    #2.-borrar calificaciones del alumno
+    borrar_calificaciones_por_id_alumno(sesion,id_alumno)
+    #3.- Select para verificar si existe el alumno
+    alum=alumnos_por_id(sesion, id_alumno)
+    #-Borramos 
+    if alum is not None:
+        #Borramos usuario
+        sesion.delete(alum)
+        #confirmar cambios
+        sesion.commit()
+    respuesta ={
+        "mensaje": "usuario eliminado"
+    }
+    return respuesta
+
+
 #Para hacer lo de fotos
 #para consultar todos las fotos
 def fotos(sesion:Session):
@@ -26,7 +48,37 @@ def fotos_por_id(sesion:Session,id_fo:int):
 # para consultar select * from app.fotos where id = id_al
 def fotos_por_id_alumno(sesion:Session,id_al:int):
     print("select * from app.fotos where id = ", id_al)
-    return sesion.query(modelos.Fotos).join(modelos.Alumnos).filter(modelos.Alumnos.id==id_al).first() #usamos join para combinar
+    return sesion.query(modelos.Fotos).filter(modelos.Fotos.id_alumno==id_al).all()#se usa all() para que muestre todos
+
+
+#Delete '/fotos/{id}'
+# delete from app.fotos where id=id_fo
+def borra_foto_por_id(sesion:Session, id_fo:int):
+    print("delete from app.foto where id=", id_fo)
+    #2.- Select para verificar si existe la foto
+    fot=fotos_por_id(sesion, id_fo)
+    #-Borramos 
+    if fot is not None:
+        #Borramos foto
+        sesion.delete(fot)
+        #confirmar cambios
+        sesion.commit()
+    respuesta ={
+        "mensaje": "foto elimiada"
+    }
+    return respuesta
+#borrar fotos por id de alumno
+#delete '/alumnos/{id}/fotos'
+#delete from app.fotos where id_alumno=id
+def borrar_fotos_por_id_alumno(sesion:Session, id_alumno:int):
+    print("delete from app.fotos where id_alumno=",id_alumno)
+    fotos_alum=fotos_por_id_alumno(sesion,id_alumno)
+    if fotos_alum is not None:
+        for fotos_alumno in fotos_alum:
+            sesion.delete(fotos_alumno)
+            sesion.commit()
+
+
 
 #para hacer lo de calificaciones
 #para consultar todos las calificaciones
@@ -44,4 +96,32 @@ def calificaciones_por_id(sesion:Session,id_ca:int):
 # para consultar select * from app.calificaciones where id = id_alumno
 def calificaciones_por_id_alumno(sesion:Session,id_al:int):
     print("select * from app.calificaciones where id = ", id_al)
-    return sesion.query(modelos.Calificaciones).join(modelos.Alumnos).filter(modelos.Alumnos.id==id_al).first() #usamos join para combinar
+    return sesion.query(modelos.Calificaciones).filter(modelos.Calificaciones.id_alumno==id_al).all() #usamos nuevamente all()
+
+#Delete '/calificaciones/{id}'
+# delete from app.calificaciones where id=id_ca
+def borra_calificaciones_por_id(sesion:Session, id_ca:int):
+    print("delete from app.calificaciones where id=", id_ca)
+    #2.- Select para verificar si existe la foto
+    cali=calificaciones_por_id(sesion, id_ca)
+    #-Borramos 
+    if cali is not None:
+        #Borramos foto
+        sesion.delete(cali)
+        #confirmar cambios
+        sesion.commit()
+    respuesta ={
+        "mensaje": "calificacion elimiada"
+    }
+    return respuesta
+
+#borrar calificaciones por id de alumno
+#delete '/alumnos/{id}/calificaciones'
+#delete from app.calificaciones where id_alumno=id
+def borrar_calificaciones_por_id_alumno(sesion:Session, id_alumno:int):
+    print("delete from app.calificaciones where id_alumno=",id_alumno)
+    calificaciones_alum=calificaciones_por_id_alumno(sesion,id_alumno)
+    if calificaciones_alum is not None:
+        for calificaciones_alumno in calificaciones_alum:
+            sesion.delete(calificaciones_alumno)
+            sesion.commit()
